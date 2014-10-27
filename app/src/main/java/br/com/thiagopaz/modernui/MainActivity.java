3b95854135a124ece7c8e36c4341812b691961e3
Thiago Paz <thiagohenrique01@gmail.com>
2014-10-27 12:25:17 -0300

    Atualização das cores dos retângulos

--- app/src/main/java/br/com/thiagopaz/modernui/MainActivity.java
+++ app/src/main/java/br/com/thiagopaz/modernui/MainActivity.java
@@ -1,17 +1,109 @@
 package br.com.thiagopaz.modernui;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SeekBar;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private String TAG = "MAIN_ACTIVITY";
+    private SeekBar seekBar;
+    private View rectRed;
+    private View rectBlue;
+    private View rectBrown;
+    private View rectWhite;
+    private View rectOrange;
+    private View rectPurple;
+    private View rectGreen;
+    private View rectYellow;
+
+    private int colorRed;
+    private int colorBlue;
+    private int colorGreen;
+    private int colorYellow;
+    private int colorBrown;
+    private int colorOrange;
+    private int colorPurple;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rectBrown = findViewById(R.id.rectBrown);
+        rectRed = findViewById(R.id.rectRed);
+        rectBlue = findViewById(R.id.rectBlue);
+        rectWhite = findViewById(R.id.rectWhite);
+        rectOrange = findViewById(R.id.rectOrange);
+        rectPurple = findViewById(R.id.rectPurple);
+        rectGreen = findViewById(R.id.rectGreen);
+        rectYellow = findViewById(R.id.rectYellow);
+
+        colorRed = Color.parseColor("#ff0000");
+        colorBlue = Color.parseColor("#0000ff");
+        colorGreen = Color.parseColor("#006700");
+        colorYellow = Color.parseColor("#FFFF00");
+        colorBrown = Color.parseColor("#a5682a");
+        colorOrange = Color.parseColor("#ffa500");
+        colorPurple = Color.parseColor("#800080");
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                modifyColor(rectRed,colorRed,progress);
+                modifyColor(rectBlue,colorBlue,progress);
+                modifyColor(rectGreen,colorGreen,progress);
+                modifyColor(rectYellow,colorYellow,progress);
+                modifyColor(rectBrown,colorBrown,progress);
+                modifyColor(rectOrange,colorOrange,progress);
+                modifyColor(rectPurple,colorPurple,progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void modifyColor(View rect, int color, int progress) {
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+
+        red -= progress;
+        green += progress;
+        blue += 2*(progress);
+
+        if(green > 255)
+            green -= 255;
+        else if(green < 0)
+            green = 255 - green;
+        if(red > 255)
+            red -= 255;
+        else if(red < 0)
+            red = 255 - red;
+        if(blue > 255)
+            blue -= 255;
+        else if(blue < 0)
+            blue = 255 - blue;
+
+        rect.setBackgroundColor(Color.rgb(red,green,blue));
     }
 
 
